aba440ca45fc1b9558977e2bb64d1b6da1579b15
Mohammad Rahman <26140599+webiondev@users.noreply.github.com>
2018-03-12 05:23:18 +0800

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('support@fundtheneedy.com'),
+    'username' => env('MAIL_USERNAME'),
 
-    'password' => env('Za1QQUm191'),
+    'password' => env('MAIL_PASSWORD'),
 
     /*
     |--------------------------------------------------------------------------
