187ad0d55539e1272f98de7ec7827238094cac1f
webiondev <sokhter@yahoo.com>
2018-03-14 04:27:48 +0800

    notify 2

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -401,7 +401,7 @@ if(($request->email==auth()->user()->email)){
         $message_new=Message::find($lastInsertedId);
         if($message->save()){
             
-            user()->notify(new NewMessage($message_new));
+            $user()->notify(new NewMessage($message_new));
             return redirect()->back()->with('message', 'message sent');
 
     }}
