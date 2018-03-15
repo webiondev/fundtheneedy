3057cbe5b82432dbe354d64f3272da60a3eb0483
webiondev <rahman_career_email1984@yahoo.com>
2018-03-16 05:51:16 +0800

    previe

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Fundtheneedy | Welcome</title>
+    <title>Fundtheneedy | Welcome<br>
+           A place where charity flourishes
+    </title>
 
     <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
     <link href="{{asset('font-awesome/css/font-awesome.css')}}" rel="stylesheet">
@@ -15,7 +17,7 @@
     <link href="{{asset('css/animate.css')}}" rel="stylesheet">
     <link href="{{asset('css/style.css')}}" rel="stylesheet">
     <meta name="description" content="Fundtheneedy is a crowdsourcing platform"/>
-    
+
 
 
 </head>
