e49fac46aecf4294159e53df4279ef3766ca04ba
webiondev <rahman_career_email1984@yahoo.com>
2018-03-21 08:12:57 +0800

    complaint

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -39,10 +39,12 @@ class WelcomeController extends Controller
     }
     public function sendexreport(Request $request){
 
+
+
         Mail::send('email', ['title' => 'Complaint', 'content' => $request->complaint], function ($message)  use ($request)
         {
 
-            $message->from($request->email, $request->name);
+            $message->from('sokhter@yahoo.com', $request->name);
             $message->subject($request->subject);
             $message->to('support@fundtheneedy.com');
 
