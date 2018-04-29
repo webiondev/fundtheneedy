021107bac01ff8076327c89b0475464a7ea5a12f
webiondev <rahman_career_email1984@yahoo.com>
2018-04-29 14:47:14 +0800

    msg

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -74,7 +74,7 @@ public function about(){
         if(Auth::check()==true and auth()->user()->type=='seeker')
             return view('seekfund');
         elseif (Auth::check()==true and auth()->user()->type=='giver')
-            return view('seeker');
+            return view('seeker2');
 
     }
 
