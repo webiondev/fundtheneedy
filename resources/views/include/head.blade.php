d65903fa8e2d606644572d23510eae6dc0d93fe8
webiondev <rahman_career_email1984@yahoo.com>
2018-04-30 18:02:27 +0800

    fillipo

--- resources/views/include/head.blade.php
+++ resources/views/include/head.blade.php
@@ -10,7 +10,8 @@
     </script>
 
 <!--  -->
-
+<meta name="verifyownership" 
+ content="61595b65c75b09a950383c94111c52fb"/>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
