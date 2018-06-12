03a049969c3aca20feffa6484a9be1138f61e4e4
webiondev <rahman_career_email1984@yahoo.com>
2018-06-12 22:08:21 +0800

    verify

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -141,4 +141,27 @@ class RegisterController extends Controller
            return $newUser;
     }
 
+
+    public function register(Request $request)
+{
+        $this->validator($request->all())->validate();
+        event(new Registered($user = $this->create($request->all())));
+        dispatch(new SendVerificationEmail($user));
+        return view(‘verification’);
+        }
+        /**
+        * Handle a registration request for the application.
+        *
+        * @param $token
+        * @return \Illuminate\Http\Response
+        */
+        public function verify($token)
+        {
+        $user = User::where('email_token',$token)->first();
+        $user->verified = 1;
+        if($user->save()){
+        return view('emailconfirm',['user'=>$user]);
+        }
+
+}
 }
