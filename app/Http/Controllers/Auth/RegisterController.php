92447f34984d7913cc28264b1046bae84af9f59a
webiondev <rahman_career_email1984@yahoo.com>
2018-06-12 22:14:36 +0800

    veri

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -147,7 +147,7 @@ class RegisterController extends Controller
         $this->validator($request->all())->validate();
         event(new Registered($user = $this->create($request->all())));
         dispatch(new SendVerificationEmail($user));
-        return view(‘verification’);
+        return view('verification');
         }
         /**
         * Handle a registration request for the application.
