f8389b2ffd86c1d4d67c90cb588d5473da569757
webiondev <rahman_career_email1984@yahoo.com>
2018-06-13 13:51:34 +0800

    verify

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -135,6 +135,15 @@ class RegisterController extends Controller
             $message->to('support@fundtheneedy.com');
 
         });
+
+       Mail::send('emailverify', ['title' => 'Verify Email', 'content' => 'Please verify your email'], function ($message)  use ($data)
+        {
+
+            $message->from('support@fundtheneedy.com', 'Fundtheneedy');
+            $message->subject('Email Verification');
+            $message->to($data['email']);
+
+        });
  
 
 
