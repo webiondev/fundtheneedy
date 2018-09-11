6419185e863fa4945fa9520e792f37158e8206e5
Mohammad Rahman <26140599+webiondev@users.noreply.github.com>
2018-09-12 02:33:00 +0800

    Update seeker2.blade.php

--- resources/views/seeker2.blade.php
+++ resources/views/seeker2.blade.php
@@ -11,15 +11,16 @@
        ?>        
 
 <h3 class="site-title">Seekers</h3>
-
+ @if (empty($data))
+                     <b>no seekers registered as of yet</b><br>
+		     <p>please check back later</p>
+                     @endif
 @foreach($data as $data_)
 <div class="container">
        
 	<div class="row">
 		<div class="col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
-		 @if (empty($data))
-                     <b>no seekers registered as of yet</b>
-                     @endif
+		
 @if ($i==0)
  <form id="seek_country"  action="{{Route('local')}}" method="GET">
        <div class="col-xs-9 col-sm-9 col-md-9">
